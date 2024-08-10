d8617bdcb06a4407e28cf432d9824d246bf557fb
Raj Jitendra Shah <141725750+Rajshah1302@users.noreply.github.com>
2024-08-11 03:16:44 +0530

    vercel deployment scripts

---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from app import app
+import os
+if __name__ == "__main__":
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    app.run(debug=True)
